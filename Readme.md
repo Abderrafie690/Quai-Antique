049d4a28e4dcccdb4cfc55af06bc8395f46de5c9
Abderrafie690 <bouejara338@hotmail.com>
2025-05-15 17:47:25 +0200

    Primer commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Quai Antique
+Ce site est un site vitrine pour le restaurant QUai Antique
+
+# Installation
---
+++ Router/allRoutes.js
@@ -0,0 +1,12 @@
+1
+import Route from "./Route.js";
+2
+3
+//Définir ici vos routes
+4
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html")
+];
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Quai Antique";
