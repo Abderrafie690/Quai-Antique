36d051c5cde57f61d225af31b3c3b3bbf526beb1
Abderrafie690 <bouejara338@hotmail.com>
2025-06-05 18:43:44 +0200

    ajout modal edit/ sup

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,12 @@ import Route from "./Route.js";
 //Définir ici vos routes
 4
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html")
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexions", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Cahangement de mot de passe", "/pages/auth/editPassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
